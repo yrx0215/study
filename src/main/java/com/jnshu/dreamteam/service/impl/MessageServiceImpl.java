953c66ef6fbcd44e67cb03e25359b78ea0a141b0
Draper_HXY <draper_hxy@163.com>
2019-03-18 17:36:34 +0800

    feat Message Quartz Job

--- src/main/java/com/jnshu/dreamteam/service/impl/MessageServiceImpl.java
+++ src/main/java/com/jnshu/dreamteam/service/impl/MessageServiceImpl.java
@@ -6,20 +6,25 @@ import com.jnshu.dreamteam.mapper.MessageMapper;
 import com.jnshu.dreamteam.pojo.Message;
 import com.jnshu.dreamteam.service.MessageService;
 import com.jnshu.dreamteam.service.QueryWrapperBaseService;
+import com.jnshu.dreamteam.service.job.MessageJob;
 import com.jnshu.dreamteam.utils.MyPage;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 
 /**
  * @author draper_hxy
  */
 @Service
-public class MessageServiceImplWrapper extends QueryWrapperBaseService<Message> implements MessageService {
+public class MessageServiceImpl extends QueryWrapperBaseService<Message> implements MessageService {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
@@ -40,12 +45,59 @@ public class MessageServiceImplWrapper extends QueryWrapperBaseService<Message> 
 
     @Override
     @Transactional
-    public Boolean insert(Message message) {
+    public Boolean insert(Message message) throws SchedulerException, InterruptedException {
         LOGGER.trace("insert message");
         Long date = System.currentTimeMillis();
         message.setCreateAt(date);
         message.setUpdateAt(date);
+
+        int messageType = message.getMessageType();
+
+        switch (messageType) {
+            case 0:
+                // TODO: 2019-03-18 立即发送消息
+                break;
+            case 1:
+                BuildMessageJob(message);
+                break;
+        }
+
+
         return getResult(messageMapper.insert(message));
+    }
+
+    private void BuildMessageJob(Message message) throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        JobDetail jobDetail = JobBuilder.newJob(MessageJob.class)
+                .storeDurably()
+                .withIdentity("Message", "SMS")
+                .build();
+
+        jobDetail.getJobDataMap().put("title", message.getTitle());
+        jobDetail.getJobDataMap().put("message", message.getMessage());
+        jobDetail.getJobDataMap().put("sendType", message.getSendType());
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity("Message", "SMS")
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 * * * * ? 2019"))
+                .build();
+
+        scheduler.scheduleJob(jobDetail, trigger);
+        scheduler.start();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MessageServiceImpl().cronBuilder(System.currentTimeMillis()));
+    }
+
+    private String cronBuilder(long time) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date(time));
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        return "0 " + minute + " " + hour + " " + month + " ? " + year;
     }
 
     @Override
