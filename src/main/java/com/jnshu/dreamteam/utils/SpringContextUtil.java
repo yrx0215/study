f5ae2145b1efdfe8c9fba79808256c2673c04dc1
Draper_HXY <draper_hxy@163.com>
2019-03-19 13:57:29 +0800

    bug Get Spring Beans in Quartz Job
    因为 Quartz 实例化 Job 的特殊方法，无法获取 bean
    避免注入失败，空指针
    添加一个工具类可以全局获取 Spring 的 Bean

---
+++ src/main/java/com/jnshu/dreamteam/utils/SpringContextUtil.java
@@ -0,0 +1,26 @@
+package com.jnshu.dreamteam.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author draper_hxy
+ */
+@Component
+public class SpringContextUtil implements ApplicationContextAware {
+
+    @Autowired
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public static Object getBean(String beanName) {
+        return applicationContext.getBean(beanName);
+    }
+}
