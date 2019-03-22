b7dba15183096a73521691c7dbc77c701ecad4af
yrx0215 <1090481058@qq.com>
2019-03-22 22:30:09 +0800

    签到 相关功能实现

--- src/main/java/com/jnshu/dreamteam/mapper/StudentMapper.java
+++ src/main/java/com/jnshu/dreamteam/mapper/StudentMapper.java
@@ -1,8 +1,9 @@
 package com.jnshu.dreamteam.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.jnshu.dreamteam.pojo.Student;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jnshu.dreamteam.pojo.StudentCheck;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -28,5 +29,26 @@ public interface StudentMapper extends BaseMapper<Student> {
                                   ,@Param("starMax") Long starMax
                                   ,@Param("studyLessonMin") Integer studyLessonMin
                                   ,@Param("studyLessonMax") Integer studyLessonMax);
+
+    /**
+     * 根据学生id查询学生签到状态
+     * @param id 学生id
+     * @return 返回值为学生的签到信息
+     */
+    StudentCheck selectStudentCheckById(Long id);
+
+    /**
+     * 插入签到信息
+     * @param studentCheck 签到信息
+     * @return 返回值为新增签到信息的id
+     */
+    Long insertStudentCheck(StudentCheck studentCheck);
+
+    /**
+     * 更新签到信息
+     * @param studentCheck 签到信息
+     * @return 返回值为true 更新成功
+     */
+    Boolean updateStudentCheck(StudentCheck studentCheck);
 
 }
