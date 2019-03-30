0a38f7ef42abc526fa443550f756e9bccdf75125
yrx0215 <1090481058@qq.com>
2019-03-30 16:43:14 +0800

    更新mission模块的更新方法

--- src/main/java/com/jnshu/dreamteam/service/MissionService.java
+++ src/main/java/com/jnshu/dreamteam/service/MissionService.java
@@ -68,5 +68,5 @@ public interface MissionService {
                                String lessonName,
                                String missionName);
 
-
+    Boolean updateMissionNameById(Mission mission);
 }
